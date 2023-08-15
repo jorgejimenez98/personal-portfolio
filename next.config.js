ebbcb4ce7711a3716d5a1a73649fbc54224d3369
Jorge Jimenez <jjimenezdiaz@quental.com>
2023-08-15 15:58:34 +0200

    feat(config): init firebase settings and log events on main layout

--- next.config.js
+++ next.config.js
@@ -3,10 +3,19 @@ const { i18n } = require("./next-i18next.config")
 
 const nextConfig = {
   i18n,
+  reactStrictMode: true,
   images: {
     domains: ['flagsapi.com'],
   },
-  reactStrictMode: true,
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_APP_DOMAIN: process.env.FIREBASE_APP_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+  }
 }
 
 module.exports = nextConfig
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import { analytics } from '../../firebase.config'
+
 import { TheHeader } from '@/components/navbars'
 import styles from './MainLayout.module.scss'
 import { UiContext } from '@/context/ui'
+import { logEvent } from 'firebase/analytics'
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { selectedTheme } = useContext(UiContext)
+  const { route } = useRouter()
+
+  useEffect(() => {
+    if (analytics) {
+      logEvent(analytics, 'page_view', { page_path: route })
+    }
+  }, [route])
 
   return (
     <div data-theme={selectedTheme}>
