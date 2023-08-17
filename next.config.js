95931626c4af7ed6fe277e007beb521f6d274722
Jorge Jimenez <georgeguitarra07@gmail.com>
2023-08-17 15:24:40 +0200

    refactor(config): delete firebase, mongodb will be used instead

--- next.config.js
+++ next.config.js
@@ -10,15 +10,7 @@ const nextConfig = {
       'media.tenor.com'
     ],
   },
-  env: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_APP_DOMAIN: process.env.FIREBASE_APP_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
-  }
+  env: { }
 }
 
 module.exports = nextConfig
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,23 +1,11 @@
-import React, { useContext, useEffect } from 'react'
-import { useRouter } from 'next/router'
-
-import { analytics } from '../../firebase.config'
-
+import React, { useContext } from 'react'
 import { TheHeader } from '@/components/navbars'
 import styles from './MainLayout.module.scss'
 import { UiContext } from '@/context/ui'
-import { logEvent } from 'firebase/analytics'
 import { ScrollToTop } from '@/components'
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { selectedTheme } = useContext(UiContext)
-  const { route } = useRouter()
-
-  useEffect(() => {
-    if (analytics) {
-      logEvent(analytics, 'page_view', { page_path: route })
-    }
-  }, [route])
 
   return (
     <div data-theme={selectedTheme}>
