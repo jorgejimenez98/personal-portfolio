caa95b6b4a867684dd63afd4d632b3b9ca37dd03
Jorge Jimenez <georgeguitarra07@gmail.com>
2023-08-22 17:14:24 +0200

    feat(main-app): init react query client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,18 @@ import '@/assets/scss/main.scss'
 import { MainLayout } from '@/layouts'
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <QueryClientProvider client={queryClient}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </QueryClientProvider>
   )
 }
 
