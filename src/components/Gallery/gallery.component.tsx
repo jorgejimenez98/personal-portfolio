e32dcd37f5c6afcb8d113819f9f8eb4425fec92d
Jorge Jimenez <georgeguitarra07@gmail.com>
2023-08-24 22:02:22 +0200

    try-fix(performance): use img instead of Next Image

--- src/components/Gallery/gallery.component.tsx
+++ src/components/Gallery/gallery.component.tsx
@@ -1,17 +1,18 @@
-import Image from 'next/image'
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 export const Gallery: React.FC<{ imageUrls: string[] }> = ({ imageUrls }) => {
   return (
     <div className='max-w-lg p-4 space-x-4 carousel carousel-center bg-neutral rounded-box'>
       {imageUrls.map((url, idx) => (
-        <Image
+        <img
           src={url}
           alt='image'
           width={200}
           height={160}
           key={idx}
           className='rounded-box bg-contain'
+          style={{ maxWidth: '100%', height: 'auto' }}
         />
       ))}
     </div>
