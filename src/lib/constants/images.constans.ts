b5ba69cba351354befb1d9e0085f0af602745c36
Jorge Jimenez <georgeguitarra07@gmail.com>
2024-05-25 17:30:11 +0200

    feat(intro): update cv url

--- src/lib/constants/images.constans.ts
+++ src/lib/constants/images.constans.ts
@@ -1,5 +1,5 @@
 export const images = {
-  resume: 'https://drive.google.com/file/d/19I3mZz4J9yQpTO5lw6mU0hiZT0kj5_BY/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/14bj_Vb8G_M2BF1EMV22-LaIz3rKSmEY_/view?usp=sharing',
   profile: 'https://firebasestorage.googleapis.com/v0/b/jorgejimenez-portfolio.appspot.com/o/foto-perfil-removebg-preview.png?alt=media&token=5349f12a-5c5c-4a7f-805a-35e2959b6ae6',
   small_profile: 'https://firebasestorage.googleapis.com/v0/b/jorgejimenez-portfolio.appspot.com/o/profile2.png?alt=media&token=5a7e3f20-7be5-4ed6-937c-03e0af417c19',
   university_logo: 'https://firebasestorage.googleapis.com/v0/b/jorgejimenez-portfolio.appspot.com/o/education%2Funiversidad-removebg-preview.png?alt=media&token=75dae1fb-fed9-4f59-bead-7dc433adf280'
